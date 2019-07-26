e07696232349d3ad762bceff2362dd82a280de1f
Hannes Kirsman <hannes.kirsman@wunder.io>
2019-07-26 09:21:34 +0300

    Proper error page if wrong author page is accessed.

--- pages/user/user-quotes.js
+++ pages/user/user-quotes.js
@@ -4,6 +4,7 @@ import headTitle from "../../lib/headTitle";
 import Quotes from '../../components/Quotes';
 import React from "react";
 import { endpoint } from '../../config';
+import Error from 'next/error';
 
 class UserQuotes extends React.Component {
 
@@ -14,19 +15,26 @@ class UserQuotes extends React.Component {
   }
 
   render() {
-    const pageTitle = 'Kasutaja ' + this.props.quotes[0].username + ' lisatud tsitaadid';
+    if (this.props.quotes.length > 0) {
+      const pageTitle = 'Kasutaja ' + this.props.quotes[0].username + ' lisatud tsitaadid';
 
-    return (
-      <div>
-        <Head>
-          <title>{headTitle(pageTitle)}</title>
-          <meta name="robots" content="noindex"/>
-        </Head>
-        <h1>{pageTitle}</h1>
-        <Quotes quotes={this.props.quotes} cookies={this.props.cookies}
-                pager={this.props.pager}/>
-      </div>
-    )
+      return (
+        <div>
+          <Head>
+            <title>{headTitle(pageTitle)}</title>
+            <meta name="robots" content="noindex"/>
+          </Head>
+          <h1>{pageTitle}</h1>
+          <Quotes quotes={this.props.quotes} cookies={this.props.cookies}
+                  pager={this.props.pager}/>
+        </div>
+      )
+    }
+    else {
+      return (
+        <Error statusCode={404} />
+      );
+    }
   }
 }
 
